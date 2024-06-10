dd4488c07a653b01ae221826d355d825316d2fa5
Lovis Bero Suchmann <lovis.suchmann@rwth-aachen.de>
2024-06-09 17:08:35 -0700

    ✨ import blog content from astro/examples/blog and adjust slightly for albums

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,13 +4,15 @@ import { css } from "@acab/ecsstatic/scss";
 import INLINE from "../utils/inline-css";
 const styled = INLINE.css;
 
-export default function Counter({
-  children,
-  count: initialCount,
+export function Counter({
+  initialCount,
+  unitSingular,
+  unitPlural,
   customColor,
 }: {
-  children: React.ReactNode;
-  count: number;
+  initialCount: number;
+  unitSingular: string;
+  unitPlural: string;
   customColor: string;
 }) {
   const [count, setCount] = useState(initialCount);
@@ -28,14 +30,13 @@ export default function Counter({
           styled`color: ${customColor};` 
         }
         className={
-          tw`grid text-4xl grid-cols-3 mt-8 place-items-center`
+          tw`grid text-4xl grid-cols-3 place-items-center`
         }
       >
         <button onClick={subtract}>-</button>
-        <pre>{count}</pre>
+        <pre>{count} {count === 1 ? unitSingular : unitPlural}</pre>
         <button onClick={add}>+</button>
       </div>
-      <div className={tw`text-center`}>{children}</div>
     </>
   );
 }
